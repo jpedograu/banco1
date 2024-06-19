b3f414b61fe2f9168168c506d02a7fe82523f910
reinato871 <ultrasenha@gmail.com>
2024-06-19 08:19:17 -0300

    sryr

--- loguin.php
+++ loguin.php
@@ -1,23 +1,8 @@
 <?php
- include"cadrasto.php";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   
-    if (isset($_POST['name']) && isset($_POST['email'])&& isset($_POST['senha'])) {
-        $nome = $_POST['username'];
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
 
-        
-        if ($username === "admin" && $password === "senha123") {
-           
-            header("Location: logou.php");
-            exit();
-        } else {
-            // Credenciais incorretas, exibe uma mensagem de erro
-            echo "Credenciais inválidas. Tente novamente.";
-        }
-    } else {
-        echo "Por favor, preencha todos os campos.";
-    }
-}
+ session_start();
+ $_SESSION["loguin"]= $loguin_provainfo;
+ $_SESSION["nome"]= $nome_provainfo;
+ $_SESSION["tipo"]= $tipo_provainfo;
+ 
 ?>
